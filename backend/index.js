46665d33f8f866a5a088cee7ec1433d6b585c153
anshsingh1032 <thansh1032@gmail.com>
2026-02-28 21:34:16 +0530

    initialize backend base with DB connection, app config, and reusable utilities

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv"
+dotenv.config()
+import { app } from "./app.js"
+import connectDB from "./db/index.js"
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log(`server is listening on ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection failed !!",err);
+    
+})
