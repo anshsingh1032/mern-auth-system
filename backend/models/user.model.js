764fd79ca7ad2ea28a90870b9815773ded3bf969
anshsingh1032 <thansh1032@gmail.com>
2026-03-01 09:36:07 +0530

    add User schema with pre-save password hashing, comparePassword and verifyToken methods

---
+++ backend/models/user.model.js
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+
+const userSchema = new mongoose.Schema(
+	{
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		name: {
+			type: String,
+			required: true,
+		},
+		lastLogin: {
+			type: Date,
+			default: Date.now,
+		},
+		isVerified: {
+			type: Boolean,
+			default: false,
+		},
+        verificationToken:{
+            type:String
+        },
+		resetPasswordToken: String,
+		resetPasswordExpiresAt: Date,
+	},
+	{ timestamps: true }
+);
+userSchema.pre("save", async function(next) {
+    if(!this.isModified("password")) return next();
+
+    this.password= await bcrypt.hash(this.password,10)
+    next()
+
+})
+userSchema.methods.isPasswordCorrect = async function (password){
+    return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateVerificationToken = function(){
+    return jwt.sign(
+        {
+        _id:this._id,
+        },process.env.VERIFICATION_TOKEN_SECRET,
+        {
+            expiresIn:process.env.VERIFICATION_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model("User", userSchema);
