50c9a55f1f6da5fb5b055d7958f9c1eee38a7ca7
anshsingh1032 <thansh1032@gmail.com>
2026-03-15 22:40:47 +0530

    fix typo

--- frontend/src/store/authSlice.js
+++ frontend/src/store/authSlice.js
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "/api/auth";
+const API_URL = import.meta.env.MODE === "development" ? "http://localhost:8000/api/auth" : "/api/auth";
 
 axios.defaults.withCredentials = true;
 
