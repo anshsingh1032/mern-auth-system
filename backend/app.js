dd082edd4139cda19fa311a8e2ad289d6f948d93
anshsingh1032 <thansh1032@gmail.com>
2026-03-15 21:50:37 +0530

    make it production ready

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cookieParser from "cookie-parser"
 import cors from "cors"
+import path from "path";
 
 const app = express()
 app.use(cors({
@@ -15,12 +16,21 @@ app.use(express.json({
 app.use(express.urlencoded({
     extended:true,limit:"16kb"
 }))
-app.use(express.static("public"))
 
 app.use(cookieParser())
 
+const __dirname = path.resolve()
+
 import userRouter from "./routes/user.routes.js"
 
-app.use("/api/v1/users",userRouter)
+app.use("/api/v1/users", userRouter)
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+	app.use((req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 export { app }
