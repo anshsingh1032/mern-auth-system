e6af76f5763a1a9005d8e77fb9a440160cd5e249
anshsingh1032 <thansh1032@gmail.com>
2026-03-15 22:05:20 +0530

    minor bug fix

--- frontend/src/store/authSlice.js
+++ frontend/src/store/authSlice.js
@@ -1,6 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-const API_URL = "http://localhost:8000/api/v1/users";
+const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "/api/auth";
+
 axios.defaults.withCredentials = true;
 
 export const signup = createAsyncThunk(
